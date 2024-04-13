62017299f6a403fa18f85846192018b4244e9150
pankaj90988 <pankajjnv2005@gmail.com>
2024-04-13 21:18:26 +0530

    navbar added

--- LifeRaiser/src/App.jsx
+++ LifeRaiser/src/App.jsx
@@ -1,15 +1,39 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import logo from './assets/logo photo-bg.jpg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import RegistrationForm from './components/registration'
-
+// import {FaBeer} from 'react-icon/fa'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <RegistrationForm />
+      <div className='outer-container'>
+        <div className="container-1">
+          <div className="container-sub-1">
+             <div className="container-sub-1-of-sub-1">
+              <img src={logo} alt="My-logho"className="my-logo" />
+             </div>
+             <div className="container-sub-2-of-sub-1">
+              <p>Browse Fundraisers</p>
+             </div>
+             <div className="container-sub-3-of-sub-1">
+              <p>Fundraise For</p>
+             </div>
+             <div className="container-sub-4-of-sub-1">
+              <p>How It Works</p>
+             </div>
+             <div className="search-icon">
+             {/* <p className='search'><FontAwesomeIcon icon="fas fa-search" /></p> */}
+              <p>Search</p>
+             </div>
+          </div>
+          <div className="container-sub-2">
+
+          </div>
+        </div>
+      </div>
     </>
   )
 }
