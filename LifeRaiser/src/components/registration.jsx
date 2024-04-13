d572f71bff845c61511c8676b9e19c15d6eb4164
shivam3050 <156913176+shivam3050@users.noreply.github.com>
2024-04-13 23:03:44 +0530

    Update registration.jsx

--- LifeRaiser/src/components/registration.jsx
+++ LifeRaiser/src/components/registration.jsx
@@ -9,6 +9,7 @@ function RegistrationForm() {
   
 
   return (
+    <div className="flex justify-center">
     <div className=''>
       <div className='flex justify-start mb-1'>
         <select className='border-2 rounded-lg p-2'>
@@ -16,45 +17,46 @@ function RegistrationForm() {
           <option value="Hindi">Hindi</option>
         </select>
       </div>
-      <div className='signupdiv w-96 rounded-lg bg-zinc-300'>
-        <h2 className='font-bold border-b-2 py-2'>Start the fundraiser</h2>
+      <div className='signupContainer my-2 border-x border-y w-96 rounded-lg bg-white'>
+        <h2 className='font-bold border-b py-2 text-center'>Start the fundraiser</h2>
         <div className="belowheading">
           <div className='flex justify-center'>
-            <div className="w-10/12 rounded-xl bg-zinc-50 my-4 py-2">
+            <div className="flex justify-center w-10/12 rounded-xl bg-zinc-200 my-4 py-2">
               Why promoting funds is important
             </div>
           </div>
           <div className="inputs">
             <div className='flex flex-col m-8'>
-              <input className='border-b-2 border-zinc-300 text-sm' type="text" name="" id="requiredName" placeholder='Name*'/>
+              <input required className='border-b border-zinc-300 text-sm py-2' type="text" name="" id="requiredName" placeholder='Name*'/>
               <label className='flex justify-start text-xs text-zinc-400' htmlFor="requiredName">Name which is mentioned in your Aadhar Card</label>
             </div>
             <div className='flex flex-col m-8'>
-              <input className='border-b-2 border-zinc-300 text-sm' type="text" name="" id="requiredName" placeholder='Email Address*'/>
+              <input className='border-b border-zinc-300 text-sm py-2' type="text" name="" id="requiredName" placeholder='Email Address*'/>
             </div>
             <div className='flex flex-col m-8'>
-              <input className='border-b-2 border-zinc-300 text-sm' type="text" name="" id="requiredName" placeholder='Create a Password*'/>
+              <input className='border-b border-zinc-300 text-sm py-2' type="text" name="" id="requiredName" placeholder='Create a Password*'/>
             </div>
             <div className='flex flex-col m-8'>
-              <div className='border-b-2 text-sm'>
-                <select>
+              <div className='border-b text-sm flex justify-evenly'>
+                <select className='py-2 w-1/6'>
                   <option value="">India</option>
                   <option value="">Pakistan</option>
                   <option value="">China</option>
                 </select>
-                <input type="text" name="" id="" placeholder='Mobile*'/>
+                <input className='py-2 w-5/6' type="text" name="" id="" placeholder='Mobile*'/>
               </div>
             </div>
-            <div className='m-6 text-sm'>
+            <div className='m-6 text-sm text-center'>
               Already have an account?<span className='text-blue-400'> Login</span>
             </div>
-            <div className="bg-cyan-300 text-white font-bold py-2">
+            <div className="rounded-b-lg bg-cyan-300 text-white font-bold py-2 text-center">
               Next step
             </div>
           </div>
         </div>
       </div>
 
+    </div>
     </div>
   );
 }
