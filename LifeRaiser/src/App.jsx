3e293da05729d636d6253bbccc9d0038cef1a576
shivam3050 <156913176+shivam3050@users.noreply.github.com>
2024-04-13 23:43:43 +0530

    Update App.jsx

--- LifeRaiser/src/App.jsx
+++ LifeRaiser/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import logo from './assets/logo photo-bg.jpg'
+import whatsappicon from './assets/whatsapp-icon.png'
+import babyphoto from './assets/baby photo.avif'
 import viteLogo from '/vite.svg'
 import './App.css'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -12,6 +14,8 @@ function App() {
   return (
     <>
       <div className='outer-container'>
+        
+        {/* Container-1 Navbar */}
         <div className="container-1">
           <div className="container-sub-1">
              <div className="container-sub-1-of-sub-1">
@@ -21,7 +25,12 @@ function App() {
               <p>Browse Fundraisers</p>
              </div>
              <div className="container-sub-3-of-sub-1">
-              <p>Fundraise For</p>
+              <select name="choose" id="choose">
+                <option value="#">Fundraise For</option>
+                <option value="a">Medical Treatment</option>
+                <option value="b">NGO/Charity</option>
+                <option value="c">Other Cause</option>
+            </select>
              </div>
              <div className="container-sub-4-of-sub-1">
               <p>How It Works</p>
@@ -34,9 +43,30 @@ function App() {
              </div>
           </div>
           <div className="container-sub-2">
-
+            <div className="whatsapp-fundraiser-sign">
+                <div className="sub-2-whatsapp-icon">
+                    <img src={whatsappicon } alt="contact" />
+                </div>
+                <div className="sub-2-fundraiser">
+                    <p>Start a Fundraiser</p>
+                </div>
+                <div className="sub-2-sign">
+                   <p>Sign In</p>
+                </div>
+              </div>
           </div>
         </div>
+      </div>
+
+
+      {/* Container-2 Baby Image*/}
+      <div className="container-2">
+         <div className="baby-big-image">
+          <img src={babyphoto} alt="Baby-photo" />
+          <div className="need-funds-heading">
+            <h1>Need Funds to Pay For a Medical Emergency or Social Cause?</h1>
+          </div>
+         </div>
       </div>
     </>
   )
