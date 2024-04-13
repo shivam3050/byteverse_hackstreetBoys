849c8b141a75d0011d204fd4b1e8ae9e922dc1c3
shivam3050 <shivamkumar30508209@gmail.com>
2024-04-13 19:55:28 +0530

    again initialised project

---
+++ LifeRaiser/src/components/registration.jsx
@@ -0,0 +1,25 @@
+
+
+import React, { useState } from 'react';
+
+function RegistrationForm() {
+
+
+
+  
+
+  return (
+    <div className=''>
+      <select>
+        <option value="English">English</option>
+        <option value="Hindi">Hindi</option>
+      </select>
+      <div className='signup'>
+        <h2 className='font-bold bg-green-500'>Start the fundraiser</h2>
+      </div>
+
+    </div>
+  );
+}
+
+export default RegistrationForm;
