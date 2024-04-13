3d4053e4acb3d8006a5af112e89cf11a59ce3b70
shivam3050 <156913176+shivam3050@users.noreply.github.com>
2024-04-14 02:20:44 +0530

    Add files via upload

---
+++ LifeRaiser/src/components/navbars.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import logo from '../assets/logo photo-bg.jpg'
+import whatsappicon from '../assets/whatsapp-icon.png'
+
+function Navbar() {
+    return(
+        <>
+        <div className="container-1 fixed top-0">
+          <div className="container-sub-1">
+             <div className="container-sub-1-of-sub-1">
+              <img src={logo} alt="My-logho"className="my-logo" />
+             </div>
+             <div className="container-sub-2-of-sub-1">
+              <p>Browse Fundraisers</p>
+             </div>
+             <div className="container-sub-3-of-sub-1">
+              <select name="choose" id="choose">
+                <option value="#">Fundraise For</option>
+                <option value="a">Medical Treatment</option>
+                <option value="b">NGO/Charity</option>
+                <option value="c">Other Cause</option>
+            </select>
+             </div>
+             <div className="container-sub-4-of-sub-1">
+              <p>How It Works</p>
+             </div>
+             <div className="container-sub-4-of-sub-1">
+              <p>|</p>
+             </div>
+             <div className="search-icon">
+              <p>Search</p>
+             </div>
+          </div>
+          <div className="container-sub-2">
+            <div className="whatsapp-fundraiser-sign">
+                <div className="sub-2-whatsapp-icon">
+                    <img src={whatsappicon } alt="contact" />
+                </div>
+                <div className="sub-2-fundraiser">
+                    <p>Start a Fundraiser</p>
+                </div>
+                <div className="sub-2-sign">
+                   <p>Sign In</p>
+                </div>
+              </div>
+          </div>
+        </div>
+        </>
+    )
+};
+
+export default Navbar;
